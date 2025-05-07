891e8e82828afb03d17c16f5babf1be8abcbe554
Jossy Solá <jossysola@gmail.com>
2025-05-06 19:53:26 -0600

    Adds metadata with captions and alt texts for the pictures

--- app/ui/gallery.tsx
+++ app/ui/gallery.tsx
@@ -10,7 +10,34 @@ export default function Gallery ({ blobs }: {
 }) {
     const [ currentPage, setCurrentPage ] = useState(1);
     const navigate = useNavigate();
+    console.log(blobs)
 
+    const metadata = [
+        {
+            caption: "Skull and Bear in clay",
+            alt: "Sculpt of a skull without jaw and a sculpt of a bear as part of a cup lid with both arms lying on the lid and his mouth open. They don't have paint."
+        },
+        {
+            caption: "Prototype of a plant pot 🌱",
+            alt: "Drawing in charcoal of a woman's torso with the four limbs cut off, headless, and herbs coming out the neck."
+        },
+        {
+            caption: "Sculpt inspired on the Chatterer from Hellraiser",
+            alt: "Sculpt in clay of a fictional character. It has no eyes, no nose, and no mouth. The teeth and gums are exposed and there is a line dividing half of the face. The space where the nose and eyes would be placed up to the top is filled with concave and irregular outlines representing tissue and veins. In the cheekbones are two concave triangles on each side. The neck has carved details giving illusion of tendons. It has a small ears and the figure is chest to head."
+        },
+        {
+            caption: "Inspired by the Harry Potter Weasley's house.",
+            alt: "Sculpt in clay of a castle with irregular and asymmetrical parts and gable roofs. The castle sits on top of a base shaped like a sea wave. The roofs have a brick orangy color whereas the rest of the castle has a light beige color. The base has a carved texture giving the illusion of the water movement, and it is painted in a Steel Blue color."
+        },
+        {
+            caption: "Woman torso as a plant pot",
+            alt: "Woman torso with no limbs and head. The body is painted in a bright red and the nipples in navy blue. The figure is carved inside so it works as a plant pot. There are two pair of holes on each side where the legs would be, and their outlines painted in navy blue. These holes are meant to help the water come out."
+        },
+        {
+            caption: "Skull in clay",
+            alt: "Sculpt in clay of a skull with no jaw. It is carved inside and it has no paint."
+        }
+    ]
     return <section className="w-full h-screen flex flex-col justify-center items-center gap-5 p-5">
         <header className="w-full flex flex-col md:flex-row justify-center items-center">
             <Button 
@@ -27,12 +54,12 @@ export default function Gallery ({ blobs }: {
             blobs && blobs[currentPage] ?
             <Card isFooterBlurred className="border-none" radius="lg">
                 <Image 
-                alt=""
+                alt={ metadata[currentPage - 1].alt ?? "" }
                 width={350}
                 src={ blobs[currentPage].url ?? "" }
                 />
                 <CardFooter className="justify-center before:bg-white/10 h-fit border-white/20 border-1 overflow-hidden py-3 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-                    <p className="text-tiny text-white/80">Caption</p>
+                    <p className="text-tiny text-white/80">{ metadata[currentPage - 1].caption ?? "" }</p>
                 </CardFooter>
             </Card> : 
             <p className="text-2xl text-slate-400">Empty</p>
