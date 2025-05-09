548b30d18c0b4b1c34d5d1c9f94cf2979c492873
Jossy Solá <jossysola@gmail.com>
2025-05-09 10:11:22 -0600

    Adds Go back button

--- app/routes/portfolio.tsx
+++ app/routes/portfolio.tsx
@@ -1,9 +1,11 @@
-import { Card, CardBody } from "@heroui/react";
+import { Button, Card, CardBody } from "@heroui/react";
 import type { Route } from "../+types/root";
 import SocialMedia from "~/ui/social-media";
 import Projects from "~/ui/projects";
 import Interests from "~/ui/interests";
 import Certifications from "~/ui/certifications";
+import { ArrowCircleLeft } from "~/ui/icons/vercel";
+import { useNavigate } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -13,9 +15,17 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Portfolio () {
+  const navigate = useNavigate()
   return (
     <section className="w-full p-5 flex flex-col items-center">
-      <header>
+      <header className="w-full flex flex-col md:flex-row justify-center items-center gap-5">
+        <Button 
+        isIconOnly 
+        aria-label="Go back button" 
+        variant="light"
+        onPress={ () => navigate(-1) }>
+          <ArrowCircleLeft />
+        </Button>
         <h1 style={{ fontFamily: "Geist Bold" }} className="text-3xl">José María Solá</h1>
       </header>
       
