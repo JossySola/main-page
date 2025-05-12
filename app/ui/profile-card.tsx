789478cf40967126bf67a188ab9156be7de65736
Jossy Solá <jossysola@gmail.com>
2025-05-12 01:39:05 -0600

    Ensures social media icons are 32 x 32 pixels

--- app/ui/profile-card.tsx
+++ app/ui/profile-card.tsx
@@ -16,14 +16,14 @@ export default function ProfileCard () {
     <p className="text-md">AuDHD left-handed alien 👽</p>
     <section className="flex flex-row justify-center items-center gap-4 mt-4">
       <Link to="https://github.com/JossySola" target="_blank">
-        <img width={32} height={32} className="dark:relative" src="github-mark-white.png" alt="GitHub" title="GitHub" aria-label="GitHub Profile" />
-        <img width={32} height={32} className="dark:hidden" src="github-mark.png" alt="GitHub" title="GitHub" aria-label="GitHub Profile" />
+        <img width={32} height={32} className="dark:relative h-[32px] object-contain" src="github-mark-white.png" alt="GitHub" title="GitHub" aria-label="GitHub Profile" />
+        <img width={32} height={32} className="dark:hidden h-[32px] object-contain" src="github-mark.png" alt="GitHub" title="GitHub" aria-label="GitHub Profile" />
       </Link>
       <Link to="https://www.instagram.com/jossysolart/" target="_blank">
         <img width={32} height={32} src="instagram.webp" alt="Instagram" title="Instagram" aria-label="Instagram Profile" />
       </Link>
       <Link to="https://www.linkedin.com/in/jossysola/" target="_blank">
-        <img width={32} height={32} src="linkedin.webp" alt="LinkedIn" title="LinkedIn" aria-label="LinkedIn Profile" />
+        <img width={32} height={32} className="h-[32px] object-contain" src="linkedin.png" alt="LinkedIn" title="LinkedIn" aria-label="LinkedIn Profile" />
       </Link>
     </section>
   </section>
--- app/ui/social-media.tsx
+++ app/ui/social-media.tsx
@@ -2,17 +2,17 @@ import { Link } from "react-router";
 
 export default function SocialMedia () {
     return (
-    <section className="flex flex-row justify-center items-center gap-4">
-      <Link to="https://github.com/JossySola" target="_blank">
-        <img width={32} height={32} className="dark:relative" src="github-mark-white.png" alt="GitHub" title="GitHub" aria-label="GitHub Profile" />
-        <img width={32} height={32} className="dark:hidden" src="github-mark.png" alt="GitHub" title="GitHub" aria-label="GitHub Profile" />
-      </Link>
-      <Link to="https://www.instagram.com/jossysolart/" target="_blank">
-        <img width={32} height={32} src="instagram.webp" alt="Instagram" title="Instagram" aria-label="Instagram Profile" />
-      </Link>
-      <Link to="https://www.linkedin.com/in/jossysola/" target="_blank">
-        <img width={32} height={32} src="linkedin.webp" alt="LinkedIn" title="LinkedIn" aria-label="LinkedIn Profile" />
-      </Link>
-    </section>
+      <section className="flex flex-row justify-center items-center gap-4">
+        <Link to="https://github.com/JossySola" target="_blank">
+          <img width={32} height={32} className="dark:relative h-[32px] object-contain" src="github-mark-white.png" alt="GitHub" title="GitHub" aria-label="GitHub Profile" />
+          <img width={32} height={32} className="dark:hidden h-[32px] object-contain" src="github-mark.png" alt="GitHub" title="GitHub" aria-label="GitHub Profile" />
+        </Link>
+        <Link to="https://www.instagram.com/jossysolart/" target="_blank">
+          <img width={32} height={32} src="instagram.webp" alt="Instagram" title="Instagram" aria-label="Instagram Profile" />
+        </Link>
+        <Link to="https://www.linkedin.com/in/jossysola/" target="_blank">
+          <img width={32} height={32} className="h-[32px] object-contain" src="linkedin.png" alt="LinkedIn" title="LinkedIn" aria-label="LinkedIn Profile" />
+        </Link>
+      </section>
     )
 }
