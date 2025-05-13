ea1822316e942e2429be63d35083594d631e86c1
Jossy Solá <jossysola@gmail.com>
2025-05-13 14:55:15 -0600

    Adds 'Currently learning...' section

--- app/ui/working-on.tsx
+++ app/ui/working-on.tsx
@@ -3,7 +3,7 @@ import { Card, CardBody, Code, Spinner } from "@heroui/react";
 export default function WorkingOn () {
     return (
         <Card classNames={{
-          base: "w-full sm:min-w-[277px] sm:w-80 h-fit sm:h-fit bg-default-100 p-5"
+          base: "w-full h-fit sm:min-w-[277px] sm:w-80 bg-default-100 p-5"
         }}>
           <CardBody className="flex flex-col gap-5">
             <h2 style={{ fontFamily: "Geist Bold" }} className="text-xl">Working on...</h2>
@@ -19,6 +19,12 @@ export default function WorkingOn () {
             <div className="flex flex-row gap-3 items-center">
               <Spinner classNames={{label: "text-foreground mt-4"}} variant="simple" color="default" />
               <p>NPM package <Code>dev</Code></p>
+            </div>
+            <h2 style={{ fontFamily: "Geist Bold" }} className="text-xl">Currently learning...</h2>
+            <div className="flex flex-row gap-3 items-center">
+              <Spinner classNames={{label: "text-foreground mt-4"}} variant="simple" color="warning" size="sm" />
+              <img src="nextjs-logotype-light-background.svg" className="inline dark:hidden" width={100} />
+              <img src="nextjs-logotype-dark-background.svg" className="hidden dark:inline" width={100} />
             </div>
           </CardBody>
         </Card>
