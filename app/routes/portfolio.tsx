c0faf670f2479a7f65446faccbebe22dbeacb11e
Jossy Solá <jossysola@gmail.com>
2025-05-22 00:04:13 -0600

    Changes Projects header size. Minor CSS changes

--- app/routes/portfolio.tsx
+++ app/routes/portfolio.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardBody } from "@heroui/react";
+import { Button, Card, CardBody, Divider } from "@heroui/react";
 import type { Route } from "../+types/root";
 import SocialMedia from "~/ui/social-media";
 import Projects from "~/ui/projects";
@@ -20,7 +20,7 @@ export default function Portfolio () {
   const navigate = useNavigate()
   return (
     <section className="w-full p-5 flex flex-col items-center">
-      <header className="w-full flex flex-col md:flex-row justify-center items-center gap-5">
+      <header className="w-full flex flex-col justify-center items-center gap-5">
         <Button 
         isIconOnly 
         aria-label="Go back button" 
@@ -29,20 +29,14 @@ export default function Portfolio () {
           <ArrowCircleLeft />
         </Button>
         <h1 style={{ fontFamily: "Geist Bold" }} className="text-3xl">José María Solá</h1>
-        <p className="text-gray-400">Fullstack web development</p>
+        <p className="text-gray-400">Fullstack web developer</p>
       </header>
       
       <section className="w-full m-5 flex flex-col md:flex-row justify-center items-center gap-5">   
-        <Card classNames={{
-          base: "w-fit h-fit bg-default-100 p-5"
-        }}>
-          <CardBody className="flex flex-col justify-center">
-            <SocialMedia />
-          </CardBody>
-        </Card>
+        <SocialMedia />
       </section>
-
-      <h2 style={{ fontFamily: "Geist Bold" }} className="text-2xl">Projects</h2>
+      <Divider className="m-5 w-full" />
+      <h2 style={{ fontFamily: "Geist Bold" }} className="text-3xl">Projects</h2>
       <section className="w-full max-w-[1000px] m-5 flex flex-col justify-center items-center gap-5">
         <Projects />
       </section>
