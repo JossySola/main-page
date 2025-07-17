d46801b314529b81bebf9448442953bc661c5f2a
Jossy Solá <jossysola@gmail.com>
2025-07-17 11:57:22 -0600

    Update versioning and working-on

--- app/ui/working-on.tsx
+++ app/ui/working-on.tsx
@@ -9,21 +9,19 @@ export default function WorkingOn () {
             <h2 style={{ fontFamily: "Geist Bold" }} className="text-xl">Working on...</h2>
             <div className="flex flex-row gap-3 items-center">
               <Spinner classNames={{label: "text-foreground mt-4"}} variant="simple" />
-              <p>Scheduler <Code>v1.0.0</Code></p>
+              <p>Fitting Room tracker <Code>dev</Code></p>
             </div>
+            
             <h2 style={{ fontFamily: "Geist Bold" }} className="text-xl">Up next...</h2>
             <div className="flex flex-row gap-3 items-center">
               <Spinner classNames={{label: "text-foreground mt-4"}} variant="simple" color="default" />
-              <p>Fitting Room tracker <Code>dev</Code></p>
+              <p>NPM package <Code>dev</Code></p>
             </div>
             <div className="flex flex-row gap-3 items-center">
               <Spinner classNames={{label: "text-foreground mt-4"}} variant="simple" color="default" />
               <p>21st Century Times <Code>v2.0.0</Code></p>
             </div>
-            <div className="flex flex-row gap-3 items-center">
-              <Spinner classNames={{label: "text-foreground mt-4"}} variant="simple" color="default" />
-              <p>NPM package <Code>dev</Code></p>
-            </div>
+            
             <h2 style={{ fontFamily: "Geist Bold" }} className="text-xl">Currently learning...</h2>
             <div className="flex flex-row gap-3 items-center">
               <Spinner classNames={{label: "text-foreground mt-4"}} variant="simple" color="warning" size="sm" />
