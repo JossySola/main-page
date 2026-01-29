0180ab08a59eee448c84f062794658c843da35ea
Jossy Solá <jossysola@gmail.com>
2026-01-28 21:37:31 -0600

    Completes rework on Gallery

--- app/ui/gallery.tsx
+++ app/ui/gallery.tsx
@@ -1,4 +1,4 @@
-import { Button, Card } from "@heroui/react";
+import { Button, Card, Tabs } from "@heroui/react";
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import { ArrowCircleLeft } from "./icons/vercel";
@@ -11,7 +11,7 @@ export default function Gallery ({ blobs }: {
     const navigate = useNavigate();
     const metadata = [
         {
-            caption: "Skull and Bear in clay",
+            caption: "Skull and Bear in clay 💀",
             alt: "Sculpt of a skull without jaw and a sculpt of a bear as part of a cup lid with both arms lying on the lid and his mouth open. They don't have paint."
         },
         {
@@ -19,19 +19,19 @@ export default function Gallery ({ blobs }: {
             alt: "Drawing in charcoal of a woman's torso with the four limbs cut off, headless, and herbs coming out the neck."
         },
         {
-            caption: "Sculpt inspired on the Chatterer from Hellraiser",
+            caption: "Sculpt inspired on the Chatterer from Hellraiser 🎬",
             alt: "Sculpt in clay of a fictional character. It has no eyes, no nose, and no mouth. The teeth and gums are exposed and there is a line dividing half of the face. The space where the nose and eyes would be placed up to the top is filled with concave and irregular outlines representing tissue and veins. In the cheekbones are two concave triangles on each side. The neck has carved details giving illusion of tendons. It has a small ears and the figure is chest to head."
         },
         {
-            caption: "Inspired by the Harry Potter Weasley's house",
+            caption: "Inspired by the Harry Potter Weasley's house 🏰",
             alt: "Sculpt in clay of a castle with irregular and asymmetrical parts and gable roofs. The castle sits on top of a base shaped like a sea wave. The roofs have a brick orangy color whereas the rest of the castle has a light beige color. The base has a carved texture giving the illusion of the water movement, and it is painted in a Steel Blue color."
         },
         {
-            caption: "Woman torso as a plant pot",
+            caption: "Woman torso as a plant pot 🌺",
             alt: "Woman torso with no limbs and head. The body is painted in a bright red and the nipples in navy blue. The figure is carved inside so it works as a plant pot. There are two pair of holes on each side where the legs would be, and their outlines painted in navy blue. These holes are meant to help the water come out."
         },
         {
-            caption: "Skull in clay",
+            caption: "Skull in clay 🧱",
             alt: "Sculpt in clay of a skull with no jaw. It is carved inside and it has no paint."
         }
     ]
@@ -47,78 +47,46 @@ export default function Gallery ({ blobs }: {
             </Button>
             <h1 style={{ fontFamily: "Geist Bold" }} className="text-center text-3xl m-5">Gallery</h1>
         </header>
-        
-        {
-            blobs && blobs[currentPage] ?
-                <Card>
+        <Card className="w-105.25 h-110 flex justify-center items-center">
+            {
+                blobs && blobs[currentPage] 
+                ? (
+                    <>
                     <img 
                     alt={ metadata[currentPage - 1].alt ?? "" }
                     width={350}
                     src={ blobs[currentPage].url ?? "" }
                     />
-                    <Card.Footer className="justify-center before:bg-white/10 h-fit border-white/20 border overflow-hidden py-2 absolute rounded-large bottom-4 w-[calc(100%-8px)] shadow-small z-10 ">
+                    <Card.Footer  className="justify-center text-center before:bg-white/10 h-fit border-white/20 border overflow-auto p-2 absolute rounded-large bottom-0.5 w-[calc(100%-75px)] shadow-small z-10 backdrop-blur-sm bg-black/10 rounded-2xl">
                         <p className="text-tiny text-white/80">{ metadata[currentPage - 1].caption ?? "" }</p>
                     </Card.Footer>
-                </Card> : 
-            <p className="text-2xl text-slate-400">Empty</p>
+                    </>
+                )
+                : null
+            }
+            
+        </Card>
+        <nav className="w-105.25">
+        {
+            <Tabs className="text-center">
+                <Tabs.ListContainer>
+                    <Tabs.List aria-label="Gallery pagination" className="w-full flex flex-row justify-center *:h-6 *:w-fit *:px-5 *:text-lg *:font-normal *:data-[selected=true]:text-accent-foreground">
+                        {
+                            blobs && blobs.map((_, index) => (
+                                <Tabs.Tab 
+                                key={ index }
+                                id={ `tab-${index + 1}` }
+                                aria-label={ `Go to page ${index + 1}` }
+                                onPress={ () => setCurrentPage(index + 1) }>
+                                    { index + 1 }
+                                    <Tabs.Indicator className="bg-accent" />
+                                </Tabs.Tab>
+                            )) 
+                        }
+                    </Tabs.List>
+                </Tabs.ListContainer>
+            </Tabs>
         }
-        
-        
-        <nav className="flex flex-row justify-center items-center gap-2">
-            <Button 
-            aria-label="previous"
-            onPress={ () => setCurrentPage(prev => (prev > 1 ? prev - 1 : prev)) }>
-                <svg 
-                aria-hidden="true" 
-                fill="none" 
-                focusable="false" 
-                height="16px" 
-                role="presentation" 
-                viewBox="0 0 24 24" 
-                width="16px">
-                    <path 
-                    d="M15.5 19l-7-7 7-7" 
-                    stroke="currentColor" 
-                    strokeLinecap="round" 
-                    strokeLinejoin="round" 
-                    strokeWidth="1.5">
-                    </path>
-                </svg>
-            </Button>
-{
-    /*
-            <Pagination 
-            color="primary" 
-            classNames={{
-                wrapper: "pl-10 pr-10 bg-default-100"
-            }}
-            size="lg"
-            initialPage={1} 
-            total={ blobs && blobs.length - 1 }
-            page={ currentPage } 
-            onChange={ setCurrentPage } />
-    */
-}
-            <Button
-            aria-label="next"
-            onPress={ () => setCurrentPage((prev) => (prev < (blobs && blobs.length - 1) ? prev + 1 : prev))}>
-                <svg 
-                aria-hidden="true" 
-                fill="none" 
-                focusable="false" 
-                height="16px" 
-                role="presentation" 
-                viewBox="0 0 24 24" 
-                width="16px" 
-                className="rotate-180">
-                    <path d="M15.5 19l-7-7 7-7" 
-                    stroke="currentColor" 
-                    strokeLinecap="round" 
-                    strokeLinejoin="round" 
-                    strokeWidth="1.5">
-                    </path>
-                </svg>
-            </Button>
         </nav>
     </section>
 }
